9ee173baf66eec4af95833772c7cbe10ef359799
“Pikadev1771” <pikadev1771@gmail.com>
2024-04-12 11:22:34 +0900

    Feat: intersection observer 활용한 무한 스크롤 구현

--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,32 +1,74 @@
+import React, { useRef, useEffect, useCallback } from 'react';
+
 import Image from 'next/image';
-import { StoreApiResponse, StoreType } from '@/interface';
+import { StoreType } from '@/interface';
+
+import { useInfiniteQuery } from 'react-query';
+
 import axios from 'axios';
-import { useQuery } from 'react-query';
 import Loading from '@/components/Loading';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
-import Pagination from '@/components/Pagination';
+
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+import Loader from '@/components/Loader';
 
 export default function StoreListPage() {
   const router = useRouter();
   const { page = '1' }: any = router.query;
+  const ref = useRef<HTMLDivElement | null>(null);
+  const pageRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!pageRef?.isIntersecting;
 
-  console.log('page', page);
+  const fetchStores = async ({ pageParam = 1 }) => {
+    const { data } = await axios('/api/stores?page=' + pageParam, {
+      params: {
+        limit: 10,
+        page: pageParam,
+      },
+    });
+
+    return data;
+  };
 
   const {
-    isLoading,
-    isError,
     data: stores,
-  } = useQuery(`stores-${page}`, async () => {
-    const { data } = await axios(`/api/stores?page=${page}`);
-    return data as StoreApiResponse;
+    isFetching,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    isError,
+    isLoading,
+  } = useInfiniteQuery('stores', fetchStores, {
+    getNextPageParam: (lastPage: any) =>
+      lastPage.data?.length > 0 ? lastPage.page + 1 : undefined,
   });
 
-  if (isError) {
-    return <span>다시 시도해 주세요</span>;
-  }
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.log(res.error);
+    }
+  }, [fetchNextPage]);
 
-  console.log('stores', stores);
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(() => {
+        fetchNext();
+      }, 500);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [fetchNext, isPageEnd, hasNextPage]);
+
+  if (isError) {
+    return (
+      <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
+        다시 시도해주세요
+      </div>
+    );
+  }
 
   return (
     <div className="px-4 md:max-w-4xl mx-auto py-8">
@@ -34,52 +76,47 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.data?.map((store, index) => (
-            <li className="flex justify-between gap-x-6 py-5" key={index}>
-              <div className="flex gap-x-4">
-                <Image
-                  src={
-                    store?.category
-                      ? `/images/markers/${store?.category}.png`
-                      : `/images/markers/default.png`
-                  }
-                  width={48}
-                  height={48}
-                  alt="아이콘 이미지"
-                />
-                <div>
-                  <div className="text-sm font-semibold leading-6 text-gray-900">
-                    {store?.name}
+          stores?.pages?.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.data.map((store: StoreType, i) => (
+                <li className="flex justify-between gap-x-6 py-5" key={i}>
+                  <div className="flex gap-x-4">
+                    <Image
+                      src={
+                        store?.category
+                          ? `/images/markers/${store?.category}.png`
+                          : '/images/markers/default.png'
+                      }
+                      width={48}
+                      height={48}
+                      alt="아이콘 이미지"
+                    />
+                    <div>
+                      <div className="text-sm font-semibold leading-6 text-gray-900">
+                        {store?.name}
+                      </div>
+                      <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                        {store?.storeType}
+                      </div>
+                    </div>
                   </div>
-                  <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                    {store?.name}
+                  <div className="hidden sm:flex sm:flex-col sm:items-end">
+                    <div className="text-sm font-semibold leading-6 text-gray-900">
+                      {store?.address}
+                    </div>
+                    <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                      {store?.phone || '번호없음'} | {store?.foodCertifyName} |{' '}
+                      {store?.category}
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="hidden sm:flex sm:flex-col sm:items-end">
-                <div className="text-sm font-semibold leading-6 text-gray-900">
-                  {store?.address}
-                </div>
-                <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                  {store?.phone || '번호없음'} | {store?.foodCertifyName} |{' '}
-                  {store?.category}
-                </div>
-              </div>
-            </li>
+                </li>
+              ))}
+            </React.Fragment>
           ))
         )}
       </ul>
-      {stores?.totalPage && (
-        <Pagination page={page} total={stores?.totalPage} />
-      )}
+      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+      <div className="w-full touch-none h-10 mb-10" ref={ref} />
     </div>
   );
 }
-
-// export async function getServerSideProps() {
-//   const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
-
-//   return {
-//     props: { stores: stores.data },
-//   };
-// }
