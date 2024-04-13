c5eca6ac3d4c3cd325ff36cb5049aa90d1df4e14
“Pikadev1771” <pikadev1771@gmail.com>
2024-04-13 21:28:54 +0900

    Feat: 검색 & 정렬 구현

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,6 +8,11 @@ import KakaoProvider from 'next-auth/providers/kakao';
 const prisma = new PrismaClient();
 
 export const authOptions: any = {
+  session: {
+    strategy: 'jwt' as const, // 세션 관리하는 방식으로 jwt 사용
+    maxAge: 60 * 60 * 24, // 세션의 최대 수명
+    updateAge: 60 * 60 * 2, // 세션 업데이트 주기
+  },
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
