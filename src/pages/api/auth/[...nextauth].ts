6285aa3ef7e6b223873acdb9578ff083752d2e7d
Park Kyung Hyun <pikadev1771@gmail.com>
2024-04-15 19:50:57 +0900

    Feat: daum 주소 검색 추가, 카카오 로그인 추가

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ import GoogleProvider from 'next-auth/providers/google';
 import NaverProvider from 'next-auth/providers/naver';
 import KakaoProvider from 'next-auth/providers/kakao';
 
-export const authOptions: any = {
+export const authOptions = {
   session: {
     strategy: 'jwt' as const, // 세션 관리하는 방식으로 jwt 사용
     maxAge: 60 * 60 * 24, // 세션의 최대 수명
@@ -19,6 +19,10 @@ export const authOptions: any = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID || '',
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || '',
     }),
   ],
   pages: {
