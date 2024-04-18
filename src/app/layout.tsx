bf63e621e22e3a946f222d652647e8590d1b243e
“Pikadev1771” <pikadev1771@gmail.com>
2024-04-18 11:13:22 +0900

    Feat: app 디렉토리 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import '@/styles/globals.css ';
+import { NextProvider, NextLayout } from './provider';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'pika-map!',
+  description: 'Next.js 14를 이용한 맛집 앱',
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <NextProvider>
+          <NextLayout>{children}</NextLayout>
+        </NextProvider>
+      </body>
+    </html>
+  );
+}
