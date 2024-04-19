8eb8ad668704c41dc60b50979d96bafed2c82f03
“Pikadev1771” <pikadev1771@gmail.com>
2024-04-19 11:13:32 +0900

    Feat: nextauth, store api를 app 디렉토리로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Map from '@/components/Map';
 import Markers from '@/components/Markers';
+
 import StoreBox from '@/components/StoreBox';
 import { StoreType } from '@/interface';
 
 export default async function Home() {
   const stores: StoreType[] = await getData();
-
+  console.log('stores', stores);
   return (
     <>
       <Map />
@@ -16,13 +17,18 @@ export default async function Home() {
 }
 
 async function getData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`, {
-    cache: 'no-store',
-  });
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`, {
+      cache: 'no-store',
+    });
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    if (!res.ok) {
+      console.log('errorrrr');
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  } catch (e) {
+    console.log(e);
   }
-
-  return res.json();
 }
